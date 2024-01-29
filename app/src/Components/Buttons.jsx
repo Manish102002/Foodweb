381ca2edf12fc7cbd9675b4c8928058fce5872bd
Manish <i.m.manish100202@gmail.com>
2024-01-29 23:32:41 +0530

    update

--- app/src/Components/Buttons.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const Buttons = ({text, onClick}) => {
-  return (
-    <button onClick={onClick} className=" py-[6px] px-[12px] rounded-sm font-semibold bg-[#FF4343] text-white ">{text}</button>
-  )
-}
-
-export default Buttons
